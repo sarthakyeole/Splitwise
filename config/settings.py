4fd4d61b4ddc8cd3eaee391b679606c57fb0e975
sarthakyeole <yeolesarthakdev@gmail.com>
2025-12-26 14:49:16 +0530

    Add Postgres config

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
