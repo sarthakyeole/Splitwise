ed9e7a1e7ef84edee5c323a1f59f65d91436e913
sarthakyeole <yeolesarthakdev@gmail.com>
2026-01-28 15:40:58 +0530

    Fix database SSL configuration for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,6 @@ if os.environ.get("DATABASE_URL"):
         'default': dj_database_url.config(
             default=os.environ.get("DATABASE_URL"),
             conn_max_age=600,
-            ssl_require=True
         )
     }
 else:
